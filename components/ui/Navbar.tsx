98054a5b78604b1c2d337874f27f477e03c85006
mohd noman <mohdnoman2751@gmail.com>
2024-06-11 23:32:14 +0530

    Add Mobile Navigation

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,8 +1,23 @@
+"use client"
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className=' flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href="/" className='flex items-center gap-1'>
+      <Image src="/icons/logo.svg" alt='streamlink' width={32} height={32} className='max-sm:size-10'/>
+      <p className='text-[26px] text-extrabold text-white max-sm:hidden'>Stream Link</p>
+      </Link>
+
+      <div className='flex-between gap-5'>
+        {/* CLERK - USER MANAGMENT  */}
+
+        <MobileNav />
+      </div>
+    </nav>
   )
 }
 
--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -12,11 +12,10 @@ const Sidebar = () => {
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px] '>
         <div className="flex flex-col gap-6">
             {sidebarLinks.map((link) => {
-                const isActve = pathname === link.route || pathname.startsWith(link.route)
+                const isActve = pathname === link.route || pathname.startsWith(`${link.route}/`)
                 return (
                   <Link href={link.route} key={link.label}
-                   className={cn("flex gap-4 items-center p-4 rounded-lg justify-start text-white",
-                    {"bg-blue-1": isActve})}
+                   className={cn("flex gap-4 items-center p-4 rounded-lg justify-start text-white",{"bg-blue-1": isActve})}
                   >
                     <Image src={link.imgUrl} alt={link.label} width={24} height={24}/>
                     {link.label}
